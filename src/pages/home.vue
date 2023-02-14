cf00889776dbba864a327c2cbc04262f4e369f33
SCOTT STUDIO <iscottt@163.com>
2023-02-14 18:16:23 +0800

    feat: 🎉 finished skill

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,13 +2,6 @@
   <div class="container">
     <div class="home-wrapper">
       <header>
-        <div class="logo">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" fill="currentColor">
-            <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
-            <path
-              d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
-          </svg>
-        </div>
         <ul class="menu">
           <li><a href="#home">首页</a></li>
           <li><a href="#skill">技能</a></li>
@@ -22,22 +15,33 @@
         <p class="desc">不知名前端攻城狮，分享编程经验和技术干货。</p>
       </div>
       <div class="more">
-        <svg t="1675434097650" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-          p-id="3446" width="200" height="200">
-          <path
-            d="M528.252661 950.942077l295.703769-289.705148c9.624195-9.380648 9.624195-24.707728 0-34.138518-9.576099-9.381671-25.222451-9.381671-34.848693 0L535.495622 875.592972 535.495622 88.901773c0-13.3214-11.048637-24.142863-24.642237-24.142863-13.597693 0-24.645306 10.821463-24.645306 24.142863l0 786.690176L232.574475 627.098411c-9.603729-9.381671-25.248034-9.381671-34.847669 0-4.836145 4.76349-7.194866 10.939143-7.194866 17.116843 0 6.1777 2.405793 12.355399 7.194866 17.116843l295.723212 289.704125c9.602705 9.383718 25.244964 9.383718 34.850739 0L528.252661 950.942077z"
-            p-id="3447"></path>
-        </svg>
+        <div class="text">SCROLL</div>
+        <div class="scrollIndicator__arrow">
+          <div data-ui="leftLine" class="leftLine" style="transform: scale(1.5, 1);"></div>
+          <div data-ui="rightLine" class="rightLine" style="transform: scale(1, 1.5);"></div>
+        </div>
       </div>
+      <div class="top-bar"></div>
+      <div class="bottom-bar"></div>
     </div>
     <section id="skill">
-      <div class="scott-title">技能</div>
+      <div class="scott-title">
+        <p class="title">技能</p>
+      </div>
+      <p class="section-info">
+        多年前端开发经验，熟练使用vue、react、nodejs、webpack、typescript、git等前端技术栈，熟悉前端工程化、组件化、模块化开发，熟悉前端性能优化、前端安全、前端工程化、前端自动化、前端架构等相关知识。
+      </p>
       <div class="skill-list">
-        <div class="favlink">
-          <img src="https://zhheo.com/img/%E6%95%B2%E6%9C%A8%E9%B1%BC.webp" alt="" />
+        <div class="favlink" v-for="item in skillList" :key="item.title">
+          <div class="icon" :style="{ background: item.bg }">
+            <img :src="`https://ethanwp.oss-cn-shenzhen.aliyuncs.com/blog/${item.title}.png`" alt="" />
+          </div>
           <div class="info">
-            <p class="title">Javascript</p>
-            <p class="desc">熟练度：奥术大师多</p>
+            <p class="title">{{ item.title }}</p>
+            <div class="desc">
+              <p class="fill" :style="{ width: item.percent + '%' }"></p>
+              <p class="out"></p>
+            </div>
           </div>
         </div>
       </div>
@@ -45,7 +49,20 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const skillList = [
+  { title: "html", percent: 80, bg: "#e9572b" },
+  { title: "css3", percent: 80, bg: "#2c51db" },
+  { title: "js", percent: 80, bg: "#f7cb4f" },
+  { title: "typescript", percent: 70, bg: "#1296db" },
+  { title: "vue", percent: 70, bg: "#fff" },
+  { title: "react", percent: 70, bg: "#e65164" },
+  { title: "nodejs", percent: 70, bg: "#1afa29" },
+  { title: "webpack", percent: 70, bg: "#fff" },
+  { title: "git", percent: 70, bg: "#8f55ba" },
+  { title: "ps", percent: 70, bg: "#001e36" },
+]
+</script>
 
 <style scoped lang="less">
 .container {
@@ -59,9 +76,30 @@
   display: flex;
   align-items: center;
   justify-content: center;
-  background: url('./../assets/bg.png') no-repeat center;
+  background: linear-gradient(131deg, #4E5868 -19%, #273344 66%);
   position: relative;
-  background-size: cover;
+
+  .top-bar,
+  .bottom-bar {
+    position: absolute;
+    left: 80px;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+
+  .top-bar {
+    top: 64px;
+    width: 992px;
+    height: 19px;
+    background-image: url('../assets/imgs/top-bar.png');
+  }
+
+  .bottom-bar {
+    top: 756px;
+    width: 771px;
+    height: 84px;
+    background-image: url('../assets/imgs/bottom-bar.png');
+  }
 
   header {
     width: 100%;
@@ -71,24 +109,6 @@
     top: 0;
     right: 0;
     z-index: 2;
-
-    .logo {
-      width: 100px;
-      height: 100px;
-      position: absolute;
-      top: 0;
-      left: 0;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-
-      svg {
-        width: 24px;
-        height: 24px;
-        fill: #fff;
-      }
-    }
 
     .menu {
       width: 100%;
@@ -101,6 +121,7 @@
       li {
         list-style: none;
         margin: 0 20px;
+        cursor: pointer;
 
         a {
           font-weight: 400;
@@ -108,22 +129,15 @@
           line-height: 0px;
           color: #fff;
           text-decoration: none;
-        }
+          opacity: .5;
+          transition: .35s;
 
-        cursor: pointer;
+          &:hover {
+            opacity: 1;
+          }
+        }
       }
     }
-  }
-
-  &::after {
-    content: '';
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    opacity: 0.65;
-    background: rgba(14, 14, 27, 1);
   }
 
   .info {
@@ -144,8 +158,11 @@
       letter-spacing: 0px;
       line-height: 88px;
       color: transparent;
-      background: linear-gradient(90deg, rgba(255, 226, 125, 1) 0%, rgba(255, 181, 67, 1) 29.17%, rgba(97, 255, 213, 1) 73.44%, rgba(63, 181, 255, 1) 99.11%);
+      background: linear-gradient(119deg, #FFFFFF -8%, #C2C4CC 12%, #FFFFFF 33%, #C2C4CC 57%, #BEC2C8 83%);
       -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      background-clip: text;
+      text-fill-color: transparent;
     }
 
     .desc {
@@ -169,25 +186,53 @@
   }
 
   .more {
-    width: 35px;
-    height: 50px;
+    bottom: 40px;
     position: absolute;
-    bottom: 20px;
-    right: 50%;
-    margin-right: -20px;
-    border-radius: 999px;
-    z-index: 2;
+    right: 60px;
     display: flex;
-    align-items: center;
-    justify-content: center;
+    font-family: moonbridge;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.1;
+    text-transform: uppercase;
     cursor: pointer;
-    border: 1px solid #fff;
     animation: 1s infinite alternate ease-in-out bounce;
 
-    svg {
-      width: 20px;
-      height: 20px;
-      fill: #fff;
+    .text {
+      position: relative;
+      margin-right: 10px;
+      color: #fff;
+    }
+
+    .scrollIndicator__arrow {
+      height: 10px;
+      overflow: visible;
+      position: relative;
+      top: -0.3em;
+      transform: rotate(-45deg);
+      width: 10px;
+
+      .leftLine,
+      .rightLine {
+        background-color: #fff;
+        position: absolute;
+      }
+
+      .leftLine {
+        bottom: 0;
+        left: 0;
+        top: 0;
+        transform-origin: left bottom;
+        width: 1px;
+      }
+
+      .rightLine {
+        bottom: 0;
+        height: 1px;
+        left: 5px;
+        right: 0;
+        transform-origin: left bottom;
+      }
     }
   }
 }
@@ -195,17 +240,72 @@
 section {
   width: 100%;
   height: 100vh;
-  background: f5f4f8;
+  background: #f5f4f8;
   padding: 30px 100px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-evenly;
 
   .scott-title {
-    font-size: 48px;
-    font-weight: bold;
+    margin: 20px 0;
+
+    .title {
+      width: 144px;
+      height: 50px;
+      opacity: 1;
+      font-size: 36px;
+      font-weight: normal;
+      line-height: 50px;
+      letter-spacing: 0px;
+      color: #0D0F1A;
+      position: relative;
+      z-index: 2;
+
+      &:after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 142px;
+        height: 25px;
+        opacity: 1;
+        background: #DADCE0;
+        z-index: -1;
+      }
+
+      &::before {
+        content: "SKILL";
+        position: absolute;
+        top: -100%;
+        opacity: 0.4;
+        font-size: 68px;
+        font-weight: normal;
+        line-height: 101px;
+        letter-spacing: 0px;
+        color: rgba(194, 197, 204, 0);
+        -webkit-text-stroke: #A6AAB2 1px;
+      }
+    }
+  }
+
+  .section-info {
+    width: 1120px;
+    height: 64px;
+    opacity: 1;
+    font-size: 18px;
+    font-weight: normal;
+    line-height: 32px;
+    letter-spacing: 0px;
+    color: #8B8F99;
+
   }
 
   .skill-list {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
+    width: 100%;
 
     .favlink {
       padding: 14px;
@@ -213,63 +313,87 @@ section {
       display: flex;
       align-items: center;
       justify-content: center;
-      background: #fff;
+      flex-direction: column;
       position: relative;
       cursor: pointer;
 
-      &:after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        border-radius: 6px;
-        transition: 0.35s;
-        z-index: 1;
-        opacity: 0;
-        background: linear-gradient(135deg, #59c3fb 10%, #268df7 100%);
+      .icon {
+        width: 100px;
+        height: 100px;
+        border-radius: 20px;
+        transform: rotate(45deg);
+        box-shadow: 40px 40px 80px 0px rgba(0, 0, 0, 0.06);
+        margin-bottom: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        img {
+          max-width: 100%;
+          transform: rotate(-45deg);
+          border-radius: 10px;
+          width: 60%;
+        }
+
       }
 
-      img {
-        width: 60px;
-        height: 60px;
-        border-radius: 999px;
-        margin-right: 10px;
-        position: relative;
-        z-index: 2;
-      }
+      // react
 
       .info {
         flex: 1;
         position: relative;
         z-index: 2;
         height: 100%;
+        width: 100%;
         display: flex;
-        align-items: flex-start;
-        justify-content: flex-start;
+        align-items: center;
+        justify-content: space-evenly;
         flex-direction: column;
 
         p {
           margin: 0;
+          margin-bottom: 10px;
           transition: 0.35s;
+          font-family: moonbridge;
 
           &.title {
-            font-weight: bold;
-            font-size: 18px;
+            width: 100%;
+            text-align: center;
+            font-size: 20px;
+            font-weight: normal;
+            line-height: 28px;
+            letter-spacing: 0px;
+            color: #0D0F1A;
+            text-transform: capitalize;
           }
-        }
-      }
 
-      &:hover {
-
-        // box-shadow: rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;
-        &:after {
-          opacity: 1;
         }
 
-        .info p {
-          color: #fff;
+        .desc {
+          width: 80%;
+          height: 50px;
+          position: relative;
+          display: flex;
+          align-items: center;
+          justify-content: flex-start;
+
+          .fill {
+            height: 10px;
+            opacity: 1;
+            background: rgba(83, 144, 235, 1);
+            border-radius: 20px 10px 10px 20px;
+            box-shadow: 0 2px 10px rgba(115, 171, 255, 0.3);
+            margin-right: 5px;
+          }
+
+          .out {
+            flex: 1;
+            height: 10px;
+            opacity: 1;
+            border-radius: 10px 20px 20px 10px;
+            box-shadow: 0 2px 10px rgba(200, 234, 234, 0.3);
+            background: rgba(200, 234, 234, 1);
+          }
         }
       }
     }
@@ -282,7 +406,7 @@ section {
   }
 
   100% {
-    transform: translateY(-10px);
+    transform: translateY(-5px);
   }
 }
 </style>
